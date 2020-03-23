9da4c7f9938b179d7d9e784cf258e3cb9d5f3bb9
lvcq <lvcq617@aliyun.com>
2020-03-23 17:53:57 +0800

    调整

--- renderer/src/pages/init/init.tsx
+++ renderer/src/pages/init/init.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import Paper from "@material-ui/core/Paper";
 import "./init.scss";
-import { TextField, Button, Snackbar, Grow } from "@material-ui/core";
+import {
+  TextField,
+  Button,
+  Snackbar,
+  Grow,
+  Backdrop,
+  CircularProgress
+} from "@material-ui/core";
 import { TransitionProps } from "@material-ui/core/transitions/transition";
 import Alert from "@material-ui/lab/Alert";
 
@@ -20,6 +27,7 @@ interface InitState {
   comfirm: FormItem;
   email: FormItem;
   noFilTip: boolean;
+  loadingIsInited: boolean;
 }
 
 class Init extends Component<{}, InitState> {
@@ -28,6 +36,7 @@ class Init extends Component<{}, InitState> {
     this.state = {
       activeStep: 0,
       noFilTip: false,
+      loadingIsInited: true,
       roleName: {
         value: "",
         error: false,
@@ -127,6 +136,10 @@ class Init extends Component<{}, InitState> {
     this.setState(Object.assign({}, this.state, { noFilTip: false }));
   }
 
+  handleLoadingClose() {
+    this.setState(Object.assign({}, this.state, { loadingIsInited: false }));
+  }
+
   componentDidMount() {}
 
   formValueChane(
@@ -145,110 +158,117 @@ class Init extends Component<{}, InitState> {
       return <Grow {...props} />;
     };
     return (
-      <Paper className="init-page" style={{ marginTop: "10%" }} elevation={3}>
-        <div className="page-title">
-          <span>管理员信息</span>
-        </div>
-        <form autoComplete="off" className="init-form">
-          <TextField
-            label="超级管理员角色名称"
-            required
-            fullWidth
-            margin="dense"
-            value={this.state.roleName.value}
-            error={this.state.roleName.error}
-            helperText={
-              this.state.roleName.error ? this.state.roleName.help : ""
-            }
-            onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
-              this.formValueChane("roleName", evt.target.value)
-            }
-          ></TextField>
-          <TextField
-            label="超级管理员账号"
-            required
-            fullWidth
-            margin="dense"
-            value={this.state.rootName.value}
-            error={this.state.rootName.error}
-            helperText={
-              this.state.rootName.error ? this.state.rootName.help : ""
-            }
-            onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
-              this.formValueChane("rootName", evt.target.value)
-            }
-          ></TextField>
-          <TextField
-            label="超级管理员密码"
-            type="password"
-            required
-            fullWidth
-            margin="dense"
-            value={this.state.password.value}
-            error={this.state.password.error}
-            helperText={
-              this.state.password.error ? this.state.password.help : ""
-            }
-            onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
-              this.formValueChane("password", evt.target.value)
-            }
-          ></TextField>
-          <TextField
-            label="确认密码"
-            type="password"
-            required
-            fullWidth
-            margin="dense"
-            value={this.state.comfirm.value}
-            error={this.state.comfirm.error}
-            helperText={this.state.comfirm.error ? this.state.comfirm.help : ""}
-            onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
-              this.formValueChane("comfirm", evt.target.value)
-            }
-          ></TextField>
-          <TextField
-            label="超级管理员邮箱"
-            fullWidth
-            margin="dense"
-            value={this.state.email.value}
-            error={this.state.email.error}
-            helperText={this.state.email.error ? this.state.email.help : ""}
-            onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
-              this.formValueChane("email", evt.target.value)
-            }
-          ></TextField>
-        </form>
-        <div className="opt-bar">
-          <Button
-            variant="contained"
-            style={{ width: "180px" }}
-            color="primary"
-            onClick={() => this.handleSubmit()}
-          >
-            提交
-          </Button>
-        </div>
-        <Snackbar
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-          open={this.state.noFilTip}
-          onClose={(event?: React.SyntheticEvent, reason?: string) =>
-            this.handleNoFillClose(event, reason)
-          }
-          TransitionComponent={GrowTransition}
-          autoHideDuration={3000}
-        >
-          <Alert
-            severity="warning"
-            elevation={6}
-            variant="filled"
+      <div className="init-page-container">
+        <Paper className="init-page" style={{ marginTop: "10%" }} elevation={3}>
+          <div className="page-title">
+            <span>管理员信息</span>
+          </div>
+          <form autoComplete="off" className="init-form">
+            <TextField
+              label="超级管理员角色名称"
+              required
+              fullWidth
+              margin="dense"
+              value={this.state.roleName.value}
+              error={this.state.roleName.error}
+              helperText={
+                this.state.roleName.error ? this.state.roleName.help : ""
+              }
+              onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                this.formValueChane("roleName", evt.target.value)
+              }
+            ></TextField>
+            <TextField
+              label="超级管理员账号"
+              required
+              fullWidth
+              margin="dense"
+              value={this.state.rootName.value}
+              error={this.state.rootName.error}
+              helperText={
+                this.state.rootName.error ? this.state.rootName.help : ""
+              }
+              onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                this.formValueChane("rootName", evt.target.value)
+              }
+            ></TextField>
+            <TextField
+              label="超级管理员密码"
+              type="password"
+              required
+              fullWidth
+              margin="dense"
+              value={this.state.password.value}
+              error={this.state.password.error}
+              helperText={
+                this.state.password.error ? this.state.password.help : ""
+              }
+              onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                this.formValueChane("password", evt.target.value)
+              }
+            ></TextField>
+            <TextField
+              label="确认密码"
+              type="password"
+              required
+              fullWidth
+              margin="dense"
+              value={this.state.comfirm.value}
+              error={this.state.comfirm.error}
+              helperText={
+                this.state.comfirm.error ? this.state.comfirm.help : ""
+              }
+              onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                this.formValueChane("comfirm", evt.target.value)
+              }
+            ></TextField>
+            <TextField
+              label="超级管理员邮箱"
+              fullWidth
+              margin="dense"
+              value={this.state.email.value}
+              error={this.state.email.error}
+              helperText={this.state.email.error ? this.state.email.help : ""}
+              onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                this.formValueChane("email", evt.target.value)
+              }
+            ></TextField>
+          </form>
+          <div className="opt-bar">
+            <Button
+              variant="contained"
+              style={{ width: "180px" }}
+              color="primary"
+              onClick={() => this.handleSubmit()}
+            >
+              提交
+            </Button>
+          </div>
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            open={this.state.noFilTip}
             onClose={(event?: React.SyntheticEvent, reason?: string) =>
               this.handleNoFillClose(event, reason)
             }
+            TransitionComponent={GrowTransition}
+            autoHideDuration={3000}
           >
-            请正确填写信息
-          </Alert>
-        </Snackbar>
-      </Paper>
+            <Alert
+              severity="warning"
+              elevation={6}
+              variant="filled"
+              onClose={(event?: React.SyntheticEvent, reason?: string) =>
+                this.handleNoFillClose(event, reason)
+              }
+            >
+              请正确填写信息
+            </Alert>
+          </Snackbar>
+        </Paper>
+        <Backdrop open={this.state.loadingIsInited} style={{zIndex:0}}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
     );
   }
 }
